ea169bff7b706af9a1b169e2307b902125fee044
wandercode <syamilsasi4s@gmail.com>
2020-10-12 17:38:01 +0530

    finished calculator app on rasor page

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace razor.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public double _Area;
+        
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        
+        }
+        public void OnPost()
+        {
+           double cash=Double.Parse(Request.Form["amount"]);
+            int time=Int32.Parse(Request.Form["period"]);
+            double rate=Double.Parse(Request.Form["interest"]);
+            double increase=cash*rate*time;
+            double submit=cash+(increase/100);
+            ViewData["Amount"]=submit;
+        }
+    }
+}
